8960fb21d6968c0f674d44a388ff6c148ed28dd5
Ibson lima <didocokurt@gmail.com>
2020-01-31 19:41:19 -0300

    Add files via upload
    server

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 const routes = require('./router/routes');
-
 app.use('/', routes);
-
-app.listen(3000, () => {
+require('dotenv');
+app.listen(process.env.PORT || 3000, () => {
     console.log('Servidor rodando na porta 3000...')
 });
+
 module.exports = app;
